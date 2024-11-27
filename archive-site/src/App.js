bbc10c013b8500f666123f36d2f16eeb8b0ffb4d
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-27 14:57:10 -0500

    app.js update

--- archive-site/src/App.js
+++ archive-site/src/App.js
@@ -3,24 +3,14 @@ import React from 'react';
 import './App.css';
 import InteractiveMap from './components/interactivemap';
 import Landing from './components/landing';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <h1>We Shall Not Be Moved - IN DEV</h1>
-        
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/map" element={<InteractiveMap />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>We Shall Not Be Moved - IN DEV</h1>
+      <Landing />
+      <InteractiveMap />
+    </div>
   );
 }
-
 export default App;
