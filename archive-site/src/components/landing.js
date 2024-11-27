a30bc66942c38e166d5144a9dce6032c0072ef70
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-27 15:38:30 -0500

    autoplay and mute video

--- archive-site/src/components/landing.js
+++ archive-site/src/components/landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = () => {
   return (
     <div className='landing'>
-      <video src="https://www.w3schools.com/html/mov_bbb.mp4" autoplay loop ></video>
+      <video src="https://www.w3schools.com/html/mov_bbb.mp4" autoPlay muted loop ></video>
         <div className='overlay'></div>
         <div className='content'>
             <h1>We shall not be moved</h1>
