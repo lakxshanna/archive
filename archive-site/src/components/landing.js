e8f48fdfcb16951f50c3fab2ce83e1509258de52
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-27 15:19:41 -0500

    video error 2

--- archive-site/src/components/landing.js
+++ archive-site/src/components/landing.js
@@ -4,7 +4,21 @@ import React from 'react'
 const Landing = () => {
   return (
     <div className='landing'>
-       <video src="https://www.w3schools.com/html/mov_bbb.mp4" autoplay loop ></video>
+      <video 
+        src="https://www.w3schools.com/html/mov_bbb.mp4" 
+        autoPlay 
+        muted 
+        loop 
+        style={{
+          width: '100%',
+          height: '100vh',
+          objectFit: 'cover', // Covers the full viewport
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+    ></video>
         <div className='overlay'></div>
         <div className='content'>
             <h1>We shall not be moved</h1>
