95b186bf5aa514648a979deb2f2b800bf08c6da1
Lakxshanna Raveendran <lakxshanna@Lakxshannas-MacBook-Pro-3.local>
2024-11-27 15:06:48 -0500

    fixed video URL

--- archive-site/src/components/landing.js
+++ archive-site/src/components/landing.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Landing = () => {
   return (
     <div className='landing'>
-        <video src={`${process.env.PUBLIC_URL}/images/DSC_0286.mp4`} autoPlay muted loop></video>
+        <video src={`${process.env.PUBLIC_URL}/DSC_0286.mp4`} autoPlay muted loop></video>
         <div className='overlay'></div>
         <div className='content'>
             <h1>We shall not be moved</h1>
